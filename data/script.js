823bc554d7fcb8899d0c80b230e499a2fcaaf784
Dérek Sirois <dereksirois@hotmail.ca>
2020-11-19 08:29:23 -0500

    senseur temperature fini

--- data/script.js
+++ data/script.js
@@ -1,13 +1,59 @@
-function rechauffe(){
+$(document).ready(function(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "rechauffe", true);
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("tempMin").value = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getTempMin", true);
     xhttp.send();
-}
-function pompe(){
     var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "pompe", true);
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("tempMax").value = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getTempMax", true);
     xhttp.send();
-}
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("tempsEntre").value = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getTempsEntre", true);
+    xhttp.send();
+    var xhttp = new XMLHttpRequest();
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            document.getElementById("tempsFiltre").value = this.responseText;
+        }
+    };
+    xhttp.open("GET", "getTempsFiltre", true);
+    xhttp.send();
+    $("#appliquer").click(function(){
+        var valeur = $("#tempMin").val();
+        $.post("setTempsMin",{
+            tempMin: valeur
+        });
+        var valeur = $("#tempMax").val();
+        $.post("setTempsMax",{
+            tempMax: valeur
+        });
+    });
+    $("#appliquerPompe").click(function(){
+        var valeur = $("#tempsEntre").val();
+        $.post("setTempsEntre",{
+            tempsEntre: valeur
+        });
+        var valeur = $("#tempsFiltre").val();
+        $.post("setTempsFiltre",{
+            tempsFiltre: valeur
+        });
+    });
+});
+
 setInterval(function getData(){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <wire.h>
 #include "SSD1306.h"
 #include <OneWire.h>
+#include <DallasTemperature.h>
+
 
 SSD1306 display(0x3c, 21, 22); 
 
@@ -12,13 +14,36 @@ const char *password = "maison123456789";
 
 AsyncWebServer server(80);
 
+// le pin pour le onewire
+const int oneWireBus = 19;     
+
+// Setup du onewire
+OneWire oneWire(oneWireBus);
+
+DallasTemperature sensors(&oneWire);
+
+int tempMin = 20;
+
+int tempMax = 25;
+
+int tempsEntre = 10;
+
+int tempsFiltre = 10;
+
+bool chauffe = false;
+
+int filtrage = 0;
+
+bool filtrageEnCours = false;
 
 void setup() {
   //serial
   Serial.begin(115200);
   display.init();
+  sensors.begin();
   //GPIO
     
+    pinMode(18, OUTPUT);
   //SPIFFS
   if(!SPIFFS.begin())
   {
@@ -54,6 +79,10 @@ void setup() {
   {
     request->send(SPIFFS, "/index.html", "text/html");
   });
+  server.on("/index_en.html", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/index_en.html", "text/html");
+  });
 
   server.on("/w3.css", HTTP_GET, [](AsyncWebServerRequest *request)
   {
@@ -64,23 +93,134 @@ void setup() {
   {
     request->send(SPIFFS, "/script.js", "text/javascript");
   });
+  server.on("/jquery-3.5.1.min.js", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send(SPIFFS, "/jquery-3.5.1.min.js", "text/javascript");
+  });
   server.on("/lireDegre", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.println("lire degre");
+    sensors.requestTemperatures(); 
+    int val = int(sensors.getTempCByIndex(0));
+    String luminosite = String(val);
+    request->send(200, "text/plain", luminosite);
+    
   });
-  server.on("/rechauffe", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.println("rechauffe");
+  server.on("/getTempMin", HTTP_GET, [](AsyncWebServerRequest *request){
+    String tempMinStr = String(tempMin);
+    request->send(200, "text/plain", tempMinStr);
+    
   });
-  server.on("/pompe", HTTP_GET, [](AsyncWebServerRequest *request){
-    Serial.println("pompe");
+  server.on("/getTempMax", HTTP_GET, [](AsyncWebServerRequest *request){
+    String tempMaxStr = String(tempMax);
+    request->send(200, "text/plain", tempMaxStr);
+    
   });
+
+  server.on("/getTempsEntre", HTTP_GET, [](AsyncWebServerRequest *request){
+    String tempsEntreStr = String(tempsEntre);
+    request->send(200, "text/plain", tempsEntreStr);
+    
+  });
+  server.on("/getTempsFiltre", HTTP_GET, [](AsyncWebServerRequest *request){
+    String tempsFiltreStr = String(tempsFiltre);
+    request->send(200, "text/plain", tempsFiltreStr);
+    
+  });
+  server.on("/setTempsMin", HTTP_POST, [](AsyncWebServerRequest *request){
+    if(request->hasParam("tempMin", true))
+    {
+      String message;
+      message = request->getParam("tempMin", true)->value();
+      tempMin = message.toInt();
+    }
+    Serial.println(tempMin);
+    request->send(204);
+  });
+  server.on("/setTempsMax", HTTP_POST, [](AsyncWebServerRequest *request){
+    if(request->hasParam("tempMax", true))
+    {
+      String message;
+      message = request->getParam("tempMax", true)->value();
+      tempMax = message.toInt();
+    }
+    Serial.println(tempMax);
+    request->send(204);
+  });
+
+  server.on("/setTempsEntre", HTTP_POST, [](AsyncWebServerRequest *request){
+    if(request->hasParam("tempsEntre", true))
+    {
+      String message;
+      message = request->getParam("tempsEntre", true)->value();
+      tempsEntre = message.toInt();
+    }
+    Serial.println(tempsEntre);
+    request->send(204);
+  });
+  server.on("/setTempsFiltre", HTTP_POST, [](AsyncWebServerRequest *request){
+    if(request->hasParam("tempsFiltre", true))
+    {
+      String message;
+      message = request->getParam("tempsFiltre", true)->value();
+      tempsFiltre = message.toInt();
+    }
+    Serial.println(tempsFiltre);
+    request->send(204);
+  });
+  
 
   server.begin();
   Serial.println("Serveur actif!");
   display.setFont(ArialMT_Plain_10);
-  display.drawString(0, 0, "AQUA2000\nip : "+ WiFi.localIP().toString() + "\nTempérature : x degré\nTemp souhaité : x degré\nChauffe: non");
+  display.drawString(0, 0, "AQUA2000\nip : "+ WiFi.localIP().toString() + "\nTempérature : x degré\nTemp minimum : "+tempMin+" degré\nChauffe: non");
   display.display();
 }
 
 void loop() {
-  
+  display.clear();
+    sensors.requestTemperatures(); 
+    int tempActuel = int(sensors.getTempCByIndex(0));
+    //Serial.println(tempActuel);
+    if(tempActuel < tempMin){
+      chauffe = true;
+      digitalWrite(18, HIGH);
+      display.setFont(ArialMT_Plain_10);
+      display.drawString(0, 0, "AQUA2000\nip : "+ WiFi.localIP().toString() + "\nTempérature : "+tempActuel+" degré\nTemp minimum : "+tempMin+" degré\nChauffe: oui");
+      display.display();
+    }
+    else if(chauffe){
+      if(tempActuel > (tempMin + ((tempMax-tempMin)/2))){
+        chauffe = false;
+        digitalWrite(18, LOW);
+      }
+      display.setFont(ArialMT_Plain_10);
+      display.drawString(0, 0, "AQUA2000\nip : "+ WiFi.localIP().toString() + "\nTempérature : "+tempActuel+" degré\nTemp minimum : "+tempMin+" degré\nChauffe: oui");
+      display.display();
+    }
+    else{
+      display.setFont(ArialMT_Plain_10);
+      display.drawString(0, 0, "AQUA2000\nip : "+ WiFi.localIP().toString() + "\nTempérature : "+tempActuel+" degré\nTemp minimum : "+tempMin+" degré\nChauffe: non");
+      display.display();
+    }
+    
+    if(filtrageEnCours){
+      if(filtrage == tempsFiltre){
+        filtrage = 0;
+        filtrageEnCours = false;
+        //desactiver
+      }
+      else{
+        filtrage++;
+      }
+    }
+    else{
+      if(filtrage == tempsEntre){
+        filtrage = 0;
+        filtrageEnCours = true;
+        //activer
+      }
+      else
+      {
+        filtrage++;
+      }
+    }
+    delay(1000);
 }
